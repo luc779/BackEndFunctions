6aae8b1d2b04ace2bf25bcff50eaa720bfcfa86a
Luc <luc.debaupte@hotmail.com>
2023-06-16 18:15:50 -0700

    Changes Names for Fucntion Class

--- SubmitUserActivites.cs
+++ SubmitUserActivites.cs
@@ -3,6 +3,7 @@ using Company.Function;
 using FirebaseAdmin.Auth;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using MySqlConnector;
 using Newtonsoft.Json;
 
@@ -73,28 +74,20 @@ namespace Company
 
             // set command and reader at the Users Table
             using MySqlCommand command = new(QUERY, connection);
-            command.Transaction = connection.BeginTransaction();
-            try
+            using MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
             {
-                using MySqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    int currID = reader.GetInt32("ID");
-                    string currUserKey = reader.GetString("UserKey");
+                int currID = reader.GetInt32("ID");
+                string currUserKey = reader.GetString("UserKey");
 
-                    bool found = BCrypt.Net.BCrypt.Verify(userKey, currUserKey);
-                    if (found) {
-                        ID = currID;
-                        break;
-                    }
+                bool found = BCrypt.Net.BCrypt.Verify(userKey, currUserKey);
+                if (found) {
+                    ID = currID;
+                    break;
                 }
-                command.Transaction.Commit();
-                return ID;
             }
-            catch (Exception) {
-                command.Transaction.Rollback();
-                throw new Exception();
-            }
+            command.Transaction.Commit();
+            return ID;
         }
         public static bool SubmitActivities(int userID, List<dynamic> transports, List<dynamic> foods, List<dynamic> utilities) {
             MySqlConnection connection = DatabaseConnecter.MySQLDatabase();
