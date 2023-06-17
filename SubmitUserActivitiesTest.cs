d549cf1d8a2ee5c9ef563dd60e02cf2d6a37d546
Luc <luc.debaupte@hotmail.com>
2023-06-16 21:34:04 -0700

    Added comment

--- SubmitUserActivitiesTest.cs
+++ SubmitUserActivitiesTest.cs
@@ -2,6 +2,6 @@ namespace BackEndFucntions
 {
     public class SubmitUserActivities
     {
-        
+        // to be completed after firt pull request completed, for new helper functions to work with SubmitUserActivity
     }
 }
