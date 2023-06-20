65badda8a2ff3f36d593fc367450551308e7b215
TseChapman <tch222000@gmail.com>
2023-06-20 16:37:09 -0700

    Copy base function response

--- GetEmissionStatistics.cs
+++ GetEmissionStatistics.cs
@@ -5,6 +5,7 @@ using FirebaseAdmin.Auth;
 using MySqlConnector;
 using NoCO2.Util;
 using Company.Function;
+using BackEndFunctions;
 
 namespace NoCO2.Function
 {
@@ -19,6 +20,45 @@ namespace NoCO2.Function
     public async Task<HttpResponseData> GetEmissionStatisticsWithUserKey(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "create-user")] HttpRequestData req)
     {
+      var responseBodyObject = new {
+        reply = "InternalError"
+      };
+      try
+      {
+        req.Body.TryParseJson<GeneralUserKeyBody>(out var requestBody);
+
+        // Get "UserKey" parameter from HTTP request as either parameter or post value
+        string userKey = requestBody?.UserKey;
+        UserRecord userRecord = await FirebaseAuth.DefaultInstance.GetUserAsync(userKey);
+        int matchedUserID = FindUser.UserFinder(userKey);
+
+        if (matchedUserID == -1)
+        {
+          // There is no user that has a matching hashed userkey from input userkey
+          responseBodyObject = new {
+            reply = "UserNotFound"
+          };
+          return await HttpResponseDataFactory.GetHttpResponseData(req, HttpStatusCode.BadRequest, responseBodyObject);
+        }
+
+        // Format the list of emissions into an object for HttpResponseData
+        var successResponseBodyObject = new {
+            reply = "Success"
+        };
+        return await HttpResponseDataFactory.GetHttpResponseData(req, HttpStatusCode.OK, successResponseBodyObject);
+      } catch (ArgumentException) {
+        responseBodyObject = new {
+          reply = "InvalidArgument"
+        };
+        return await HttpResponseDataFactory.GetHttpResponseData(req, HttpStatusCode.BadRequest, responseBodyObject);
+      } catch (FirebaseAuthException) {
+        responseBodyObject = new {
+          reply = "UserKeyNotAuth"
+        };
+        return await HttpResponseDataFactory.GetHttpResponseData(req, HttpStatusCode.BadRequest, responseBodyObject);
+      } catch (Exception) {
+        return await HttpResponseDataFactory.GetHttpResponseData(req, HttpStatusCode.InternalServerError, responseBodyObject);
+      }
       throw new NotImplementedException();
     }
   }
