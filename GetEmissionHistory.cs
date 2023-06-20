ae488f1b23fa142765213f2e8b653e348dbc617e
TseChapman <tch222000@gmail.com>
2023-06-19 18:40:32 -0700

    Limit the use of using keyword

--- GetEmissionHistory.cs
+++ GetEmissionHistory.cs
@@ -84,51 +84,47 @@ namespace NoCO2.Function
                " ORDER BY e.DateTime ASC";
 
 
-        using (MySqlCommand command = connection.CreateCommand())
+        using MySqlCommand command = connection.CreateCommand();
+        command.CommandText = query;
+        command.Parameters.AddWithValue("@userId", userId);
+        command.Parameters.AddWithValue("@oneYearAgo", oneYearAgo.ToString("yyyy/MM/dd"));
+        command.Parameters.AddWithValue("@currentDate", currentDate.ToString("yyyy/MM/dd"));
+
+        using MySqlDataReader reader = await command.ExecuteReaderAsync();
+        List<DailyEmission> emissions = new ();
+        const string DATE_TIME_COL = "DateTime";
+        const string TOTAL_AMOUNT_COL = "TotalAmount";
+        const string GOAL_COL = "Goal";
+
+        if (reader.HasRows)
         {
-          command.CommandText = query;
-          command.Parameters.AddWithValue("@userId", userId);
-          command.Parameters.AddWithValue("@oneYearAgo", oneYearAgo.ToString("yyyy/MM/dd"));
-          command.Parameters.AddWithValue("@currentDate", currentDate.ToString("yyyy/MM/dd"));
-
-          using (MySqlDataReader reader = await command.ExecuteReaderAsync())
+          while (reader.Read())
           {
-            List<DailyEmission> emissions = new ();
-            const string DATE_TIME_COL = "DateTime";
-            const string TOTAL_AMOUNT_COL = "TotalAmount";
-            const string GOAL_COL = "Goal";
+            // Store a list of DailyEmission
+            DateTime dateTime = reader.GetDateTime(DATE_TIME_COL).Date;
+            double total = reader.GetDouble(TOTAL_AMOUNT_COL);
+            double goal = reader.GetDouble(GOAL_COL);
 
-            if (reader.HasRows)
-            {
-              while (reader.Read())
-              {
-                // Store a list of DailyEmission
-                DateTime dateTime = reader.GetDateTime(DATE_TIME_COL).Date;
-                double total = reader.GetDouble(TOTAL_AMOUNT_COL);
-                double goal = reader.GetDouble(GOAL_COL);
-
-                emissions.Add(new DailyEmission { DateTime = dateTime, Total = total, Goal = goal });
-              }
-
-              // Fill in the missing days with null total and default goal
-              const int ZERO_DATE_OFFSET = 0;
-              List<DateTime> allDates = Enumerable.Range(ZERO_DATE_OFFSET, (currentDate - oneYearAgo).Days)
-                  .Select(offset => oneYearAgo.AddDays(offset).Date)
-                  .ToList();
-
-              List<DateTime> existingDates = emissions.ConvertAll(new Converter<DailyEmission, DateTime>(ConvertToDateTime));
-
-              List<DateTime> missingDates = allDates.Except(existingDates).ToList();
-
-              emissions.AddRange(missingDates.ConvertAll(new Converter<DateTime, DailyEmission>(ConvertToEmptyEmission)));
-              connection.Close();
-              return emissions.OrderBy(e => e.DateTime).ToList();
-            }
-            // Return empty history if there are none within an year
-            connection.Close();
-            return emissions;
+            emissions.Add(new DailyEmission { DateTime = dateTime, Total = total, Goal = goal });
           }
+
+          // Fill in the missing days with null total and default goal
+          const int ZERO_DATE_OFFSET = 0;
+          List<DateTime> allDates = Enumerable.Range(ZERO_DATE_OFFSET, (currentDate - oneYearAgo).Days)
+              .Select(offset => oneYearAgo.AddDays(offset).Date)
+              .ToList();
+
+          List<DateTime> existingDates = emissions.ConvertAll(new Converter<DailyEmission, DateTime>(ConvertToDateTime));
+
+          List<DateTime> missingDates = allDates.Except(existingDates).ToList();
+
+          emissions.AddRange(missingDates.ConvertAll(new Converter<DateTime, DailyEmission>(ConvertToEmptyEmission)));
+          connection.Close();
+          return emissions.OrderBy(e => e.DateTime).ToList();
         }
+        // Return empty history if there are none within an year
+        connection.Close();
+        return emissions;
       }
     }
 
