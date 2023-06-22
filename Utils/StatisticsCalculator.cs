7c52c45631bae2a6963898ae35a9e4bda2eb12c4
TseChapman <tch222000@gmail.com>
2023-06-22 13:05:15 -0700

    Add query read on activities's activity type and emission

--- Utils/StatisticsCalculator.cs
+++ Utils/StatisticsCalculator.cs
@@ -1,7 +1,12 @@
+using MySqlConnector;
+using Company.Function;
+using System.Collections.Generic;
+
 namespace NoCO2.Util;
 
 internal class StatisticsCalculator
 {
+  // Precondition: userID is found from FindUser.UserFinder
   public List<EmissionStatistic> GetUserEmissionStatistics(int userID)
   {
     List<EmissionStatistic> statistics = new();
@@ -10,6 +15,42 @@ internal class StatisticsCalculator
 
   public EmissionStatistic GetHighestEmissionActivityByUserID(int userID)
   {
+    DateTime currentDate = DateTime.UtcNow;
+    DateTime oneWeekAgo = currentDate.AddDays(-7);
+
+    MySqlConnection connection = DatabaseConnecter.MySQLDatabase();
+
+    using (connection)
+    {
+      connection.Open();
+
+      const string query = "SELECT a.ActivityType, a.Emission" +
+        "FROM Activities AS a" +
+        "JOIN Users AS u ON a.UserID = u.ID " +
+        "WHERE u.ID = @userId AND a.DateTime >= '@oneWeekAgo' AND a.DateTime <= '@currentDate'";
+
+      using MySqlCommand command = connection.CreateCommand();
+      command.CommandText = query;
+      command.Parameters.AddWithValue("@userId", userID);
+      command.Parameters.AddWithValue("@oneWeekAgo", oneWeekAgo.ToString("yyyy/MM/dd"));
+      command.Parameters.AddWithValue("@currentDate", currentDate.ToString("yyyy/MM/dd"));
+
+      using MySqlDataReader reader = command.ExecuteReader();
+      if (reader.HasRows)
+      {
+        Dictionary<string, double> emissionsDict = new();
+        emissionsDict.Add("transport", 0);
+        emissionsDict.Add("foods", 0);
+        emissionsDict.Add("utility", 0);
+        while (reader.Read())
+        {
+          string activityType = reader.GetString("ActivityType");
+          if (emissionsDict.ContainsKey(activityType)) {
+            emissionsDict[activityType] += reader.GetDouble("Emission");
+          }
+        }
+      }
+    }
     return new EmissionStatistic {Statistic = null, Topic = null, Stat = null};
   }
 }
