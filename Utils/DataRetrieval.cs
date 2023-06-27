69810ed933ed0e38336e9e865e3f8d969e7ca585
Luc <luc.debaupte@hotmail.com>
2023-06-26 17:13:32 -0700

    Updates query

--- Utils/DataRetrieval.cs
+++ Utils/DataRetrieval.cs
@@ -7,8 +7,10 @@ namespace BackEndFucntions
     {
         public List<dynamic> RetrieveCertainType(int userID, DateTime today, string ACTIVITY_TYPE)
         {
-            List<dynamic> Transports = null;
-            string QUERY = "SELECT FROM Activities WHERE UserID = '" + userID + "' AND DateTime = '" + today.ToString("yyyy/MM/dd") + "' AND ActivityType = '" + ACTIVITY_TYPE + "'";
+            List<dynamic> Activity = null;
+            string QUERY = "SELECT e.Method, e.Amount FROM Activites AS e "+
+                "JOIN Users As u ON e.UsersID = u.iD"+
+                "' WHERE u.ID = '" + userID + "' AND e.DateTime = '" + today.ToString("yyyy/MM/dd") + "' AND ActivityType = '" + ACTIVITY_TYPE + "'";
 
             using MySqlConnection connetion = DatabaseConnecter.MySQLDatabase();
             connetion.Open();
@@ -16,14 +18,13 @@ namespace BackEndFucntions
             using MySqlCommand command = new(QUERY, connetion);
 
             try {
-                command.CommandText = QUERY;
                 using MySqlDataReader reader = command.ExecuteReader();
                 while (reader.Read()) {
                     string type = reader.GetString("Method");
                     double amount = reader.GetDouble("Amount");
-                    Transports.Add(new {Type = type, Amount = amount});
+                    Activity.Add(new {Type = type, Amount = amount});
                 }
-                return Transports;
+                return Activity;
             }
             catch (Exception) {
                 throw new Exception();
